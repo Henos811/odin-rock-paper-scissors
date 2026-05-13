21bb7288b8abb553fef4976edd095a1770c37b91
Henos <henos811@gmail.com>
2026-05-13 18:44:53 +0300

    create getHumanChoice function as well as humanScore and computerScore variables

--- script.js
+++ script.js
@@ -1,7 +1,15 @@
+let humanScore = 0;
+let computerScore = 0;
+
 function getComputerChoice() {
   const options = ["rock", "paper", "scissors"];
 
   return options[Math.floor(Math.random() * 3)];
 }
 
+function getHumanChoice(input) {
+  return input;
+}
 
+const input = window.prompt("Enter a move: ");
+console.log(getHumanChoice(input));
